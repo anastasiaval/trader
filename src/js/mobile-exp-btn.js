cc3335c011e419be4cae3c5074a4ea6f053dc6ca
anastasiaval <36269769+anastasiaval@users.noreply.github.com>
2019-09-15 13:22:05 +0300

    mobile-expanded upd

--- src/js/mobile-exp-btn.js
+++ src/js/mobile-exp-btn.js
@@ -1,19 +1,41 @@
 $(function () {
     let $expBtn = $('.interest__btn');
     $expBtn.on('click', e => {
-        $('html,body').animate({scrollTop:$(e.currentTarget).offset().top-10},{duration:1E3});
-
-        let $arrow = $(e.currentTarget).find('.interest__arrow');
-        $arrow.toggleClass('interest__arrow_up');
-
-        let $expDesc = $(e.currentTarget).next('.interest__desc');
-        $expDesc.slideToggle(500);
+        $('html,body').animate({scrollTop:$(e.currentTarget).offset().top-10},{duration:1000});
 
         $(e.currentTarget).toggleClass('expanded');
+        let $expDesc = $(e.currentTarget).next('.interest__desc');
+        let $arrow = $(e.currentTarget).find('.interest__arrow');
+        $expDesc.slideToggle(500);
+        $arrow.toggleClass('interest__arrow_up');
+
+        $expBtn.each(function() {
+            if ($(this).hasClass('expanded') && this !== e.currentTarget) {
+                $(this).removeClass('expanded');
+                $(this).find('.interest__arrow').removeClass('interest__arrow_up');
+                $(this).next('.interest__desc').slideUp(500);
+            }
+        });
+
         let $body = $('body');
         $expBtn.hasClass('expanded')
             ? $body.css('background', '#f3f2da')
             : $body.css('background', 'url("img/bg-mobile.jpg") no-repeat');
+    });
+
+    $(document).mouseup(e => {
+        let $desc = $expBtn.next('.interest__desc');
+
+        if (!$expBtn.is(e.target)
+            && $expBtn.has(e.target).length === 0
+            && !$desc.is(e.target)
+            && $desc.has(e.target).length === 0
+        ) {
+            $expBtn.find('.interest__arrow').removeClass('interest__arrow_up');
+            $desc.slideUp(500);
+            $('body').css('background', 'url("img/bg-mobile.jpg") no-repeat');
+        }
+
     });
 });
 
